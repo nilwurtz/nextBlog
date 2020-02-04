6124ef4546688b9ccd3ed2b9a4d614f261227cf6
ragnar <ragnar@ragnarPC.local>
2020-02-05 00:42:57 +0900

    Social Links

---
+++ src/components/social/GitHubIcon.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import styled from 'styled-components';
+
+import { SocialUrls } from '../../config/social';
+
+export const GitHubIcon: React.FC = () => {
+  return (
+    <a href={SocialUrls.github}>
+      <img src="/gh_icon.png" alt="twitter icon" width="50px" height="50px" />
+    </a>
+  );
+};
---
+++ src/config/social.ts
@@ -0,0 +1,4 @@
+export const SocialUrls = {
+  twitter: "https://twitter.com/Ela_ragnar",
+  github: "https://github.com/ragnar1904",
+};
