074d432208a2e103bc37bcd77c666d24eaa23ca8
ragnar <ragnar@ragnarPC.local>
2020-02-15 22:10:23 +0900

    fix app

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,21 +1,13 @@
 import 'ress';
 import '../static/style.css';
 
-import App, { AppContext } from 'next/app';
+import App from 'next/app';
 import Head from 'next/head';
 import React from 'react';
 
 import { NavBar } from '../containers/NavBar';
 
 export default class extends App {
-  static async getInitialProps({ Component, ctx }: AppContext) {
-    let pageProps = {};
-    if (Component.getInitialProps) {
-      pageProps = await Component.getInitialProps(ctx);
-    }
-    return { pageProps };
-  }
-
   render() {
     const { Component, pageProps } = this.props;
     return (
