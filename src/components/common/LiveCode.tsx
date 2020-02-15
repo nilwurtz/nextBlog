b085e052bfff9a336867318f7fa6160a0d5a936b
ragnar <ragnar@ragnarPC.local>
2020-02-15 22:02:20 +0900

    TopPage

---
+++ src/components/common/LiveCode.tsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import styled from 'styled-components';
+
+import { useStep } from '../../hooks/useStep';
+import { CodeBlock } from '../Post/MarkDownViewer';
+
+type Props = {
+  code: string;
+  language: string;
+};
+
+export const LiveCode: React.FC<Props> = props => {
+  const codeState = useStep(props.code, 35);
+  return (
+    <Root>
+      <CodeBlock value={codeState} language={props.language} />
+    </Root>
+  );
+};
+
+const Root = styled.div`
+  display: flex;
+  border: 1px solid gray;
+  overflow: scroll;
+  & > pre {
+    margin: 0;
+    flex-grow: 2;
+    width: 100%;
+  }
+`;
