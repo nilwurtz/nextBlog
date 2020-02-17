5687e54ef13f75704b7fa53e616c57ec02f1d9e8
ragnar <ragnar@ragnarPC.local>
2020-02-17 21:01:59 +0900

    fix

--- src/components/head/Analytics.tsx
+++ src/components/head/Analytics.tsx
@@ -6,7 +6,12 @@ export const GoogleAnalytics: React.FC = () => {
       <script async src={`https://www.googletagmanager.com/gtag/js?id=${process.env.GA_TRACKING_ID}`}></script>
       <script
         dangerouslySetInnerHTML={{
-          __html: `(function(){ window.dataLayer = window.dataLayer || [];function gtag(){dataLayer.push(arguments);}gtag('js', new Date());gtag('config', '${process.env.GA_TRACKING_ID}');})`,
+          __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${process.env.GA_TRACKING_ID}');
+          `,
         }}
       />
     </>
